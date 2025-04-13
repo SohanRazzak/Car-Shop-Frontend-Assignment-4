1b2ce41c3c01b50e26aeb13938ac53eb185f07c6
Sohan Razzak <>
2025-04-13 16:38:49 +0600

    minor fix

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -10,7 +10,7 @@ const baseQuery = fetchBaseQuery({
         if(token){
             headers.set('authorization', `${token}`)
         }
-        // return headers;
+        return headers;
     }
 })
 
